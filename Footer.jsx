9b0c81c6e868d56a20e3f7bbd812de42bdc90384
VISAAGAN P J <85603277+VISAAGAN-P-J@users.noreply.github.com>
2021-06-24 20:09:52 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <p>
+      <footer> Copyright by ShapeAI at {new Date().getFullYear()} </footer>
+    </p>
+  );
+}
+
+export default Footer;
